84239e72e7f3c8ffd59b0126ad6b2dea43b0c124
DongJinLee <dlehdwls21@naver.com>
2024-07-03 23:36:34 +0900

    :sparkles: Feat: solved ac 새싹 난이도 입력과 계산 백준 1001번 문제

---
+++ src/solved/새싹/입력과계산/BackJun1001.java
@@ -0,0 +1,17 @@
+package solved.새싹.입력과계산;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BackJun1001 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String[] s = bufferedReader.readLine().split(" ");
+
+        int a = Integer.parseInt(s[0]);
+        int b = Integer.parseInt(s[1]);
+
+        System.out.println(a - b);
+    }
+}
